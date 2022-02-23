a2eca5f986626c76291bb5db87c36267c49ede69
Alex Luc Taylor <godshiva@gmail.com>
2022-02-22 21:50:27 -0700

    properly using fixture now

--- fixtures/resources.py
+++ fixtures/resources.py
@@ -1,5 +1,24 @@
 import pytest
+from selenium import webdriver
+import platform
+from selenium.webdriver.chrome.service import Service
 
-@pytest.fixture
-def sample_fixture():
-    return "a"
+
+@pytest.fixture(scope="session")
+def is_mac():
+    return f"{platform.mac_ver()[0]}".strip() != ''
+
+
+@pytest.fixture(scope="function")
+def seleniumdriver(is_mac):
+
+    if is_mac:
+        s = Service('./mac/chromedriver')
+    else:
+        s = Service('./windows/chromedriver.exe')
+
+    driver = webdriver.Chrome(service=s)
+    driver.maximize_window()
+    yield driver
+    driver.close()
+
--- tests/test_website.py
+++ tests/test_website.py
@@ -1,9 +1,16 @@
+import time
+
 import pytest
 
 
 
-def test_hello():
-    assert False, "This should fail"
+def test_yahoo(seleniumdriver):
+    seleniumdriver.get("https://www.yahoo.com/")
+    time.sleep(3)
+    assert True
 
-def test_hello2(sample_fixture):
-    assert sample_fixture=='a', "This should pass"
+
+def test_google(seleniumdriver):
+    seleniumdriver.get("https://www.google.com/")
+    time.sleep(3)
+    assert True
